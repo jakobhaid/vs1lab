de184266ac472ed84a35e5898c78e266d193d1c1
jakob <jakob.haidlauf@gmail.com>
2024-06-09 18:48:05 +0000

    Aufgabe3.2.2b

--- Aufgabe3/gta_v3/public/javascripts/geotagging.js
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -13,69 +13,80 @@ console.log("The geoTagging script is going to start...");
 //import { MapManager } from "./map-manager.js";
 
 function updateLocation(){
-    // Auslesen der Position mit `findLocation`
-    // Pfeil bedeutet 
-    LocationHelper.findLocation(function(location) {
-        var latitude = location.latitude;
-        var longitude = location.longitude;
+    const previousLat = document.getElementById("latitude").value;
+    const previousLong = document.getElementById("longitude").value;
+    
+    // Position schon eingetragen?
+    if (previousLat == "" || previousLong == "") {
+        // Koordinaten sind nicht gesetzt, also finde die aktuelle Position
+        LocationHelper.findLocation((locationHelper) => {
+            var latitude = locationHelper.latitude;
+            var longitude = locationHelper.longitude;
 
-        // Koordinaten in die Formulare eintragen
-        document.getElementById('latitude').value = latitude;
-        document.getElementById('longitude').value = longitude;
-        document.getElementById('sLatitude').value = latitude;
-        document.getElementById('sLongitude').value = longitude;
+            // Koordinaten in die Formulare eintragen
+            document.getElementById("latitude").value = latitude;
+            document.getElementById("longitude").value = longitude;
+            document.getElementById('sLatitude').value = latitude;
+            document.getElementById('sLongitude').value = longitude;
 
+            // Aktualisiere die Karte mit den neuen Koordinaten und Markern
+            updateMapAndMarkers(latitude, longitude);
+        });
+    } else {
+        updateMapAndMarkers(previousLat, previousLong);
+    }
+}
 
-        /*
-        Vorhandenes Kartenelement durch neues <div>-Element mit ID 'map' ersetzt
-        und dem entsprechenden Elternelement im DOM hinzugefügt.
-        */
-        document.getElementById('mapView').nextElementSibling.remove();
-        document.getElementById('mapView').remove();
-        var mapDiv = document.createElement('div');
-        mapDiv.setAttribute("id", "map");
-        document.getElementsByClassName('discovery__map')[0].appendChild(mapDiv);
+function updateMapAndMarkers(latitude, longitude) {
+    /*
+    Vorhandenes Kartenelement durch neues <div>-Element mit ID 'map' ersetzt
+    und dem entsprechenden Elternelement im DOM hinzugefügt.
+    */
+    document.getElementById('mapView').nextElementSibling.remove();
+    document.getElementById('mapView').remove();
+    var mapDiv = document.createElement('div');
+    mapDiv.setAttribute("id", "map");
+    document.getElementsByClassName('discovery__map')[0].appendChild(mapDiv);
 
-        // neue Instanz von MapManager
-        var map = new MapManager();
+    // neue Instanz von MapManager
+    var map = new MapManager();
 
-        // Initialisiert Karte
-        map.initMap(latitude,longitude);
+    // Initialisiert Karte
+    map.initMap(latitude,longitude);
 
-        // Alle <li>-Elemente innerhalb des Elements mit der ID 'discoveryResults' werden abgerufen 
-        // und in der Variablen allResults gespeichert.
-        var allResults = document.getElementById('discoveryResults').getElementsByTagName('li');
+    // Alle <li>-Elemente innerhalb des Elements mit der ID 'discoveryResults' werden abgerufen 
+    // und in der Variablen allResults gespeichert.
+    var allResults = document.getElementById('discoveryResults').getElementsByTagName('li');
 
-        // leeres Array
-        var tagList = Array();
+    // leeres Array
+    var tagList = Array();
 
-        // Schleife durch die li Elemente
-        for (var i = 0; i < allResults.length; i++) {
+    // Schleife durch die li Elemente
+    for (var i = 0; i < allResults.length; i++) {
 
-            //Für jedes <li>-Element wird der Textinhalt (innerText) abgerufen und in der Variablen listElement gespeichert. 
-            var listElement = allResults[i].innerText;
+        //Für jedes <li>-Element wird der Textinhalt (innerText) abgerufen und in der Variablen listElement gespeichert. 
+        var listElement = allResults[i].innerText;
 
-            // Ortsname, Breiten- und Längengrad wird aus Textinhalt extrahiert
-            var listName = listElement.substring(0, listElement.indexOf('(') - 1);
-            var listLatitudeStr = listElement.substring(listElement.indexOf('(') + 2, listElement.indexOf(','));
-            var listLongitudeStr = listElement.substring(listElement.indexOf(',') + 1, listElement.indexOf(')'));
-            
-            // Objekt listTag mit Ortsnamen, Breiten- und Längengrad
-            var listTag = {
-                name: listName,
-                location:{
-                    latitude: listLatitudeStr,
-                    longitude: listLongitudeStr
-                }
-            };
+        // Ortsname, Breiten- und Längengrad wird aus Textinhalt extrahiert
+        var listName = listElement.substring(0, listElement.indexOf('(') - 1);
+        var listLatitudeStr = listElement.substring(listElement.indexOf('(') + 2, listElement.indexOf(','));
+        var listLongitudeStr = listElement.substring(listElement.indexOf(',') + 1, listElement.indexOf(')'));
+        
+        // Objekt listTag mit Ortsnamen, Breiten- und Längengrad
+        var listTag = {
+            name: listName,
+            location:{
+                latitude: listLatitudeStr,
+                longitude: listLongitudeStr
+            }
+        };
 
-            //Fügt listTag in unser Array tagList ein
-            tagList.push(listTag);
-        }
+        //Fügt listTag in unser Array tagList ein
+        tagList.push(listTag);
+    }
 
-        // Marker auf der Karte wird aktualisiert 
-        map.updateMarkers(latitude,longitude,tagList);
-    });
+    // Marker auf der Karte wird aktualisiert 
+    map.updateMarkers(latitude,longitude,tagList);
 }
 
 // Wait for the page to fully load its DOM content, then call updateLocation
